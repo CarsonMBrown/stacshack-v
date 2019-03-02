9eca0206305fe357b6dc4ede84183af48d6be6de
Ben Claydon <bc89@st-andrews.ac.uk>
2019-03-02 12:29:48 +0000

    Canvas Example

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,46 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+	@Override
+	public void start(Stage theStage) throws Exception {
+		theStage.setTitle("Canvas Example");
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+		Group root = new Group();
+		Scene theScene = new Scene(root);
+		theStage.setScene(theScene);
+
+		Canvas canvas = new Canvas(400, 200);
+		root.getChildren().add(canvas);
+
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+
+		gc.setFill(Color.RED);
+		gc.setStroke(Color.BLACK);
+		gc.setLineWidth(2);
+		Font theFont = Font.font("Times New Roman", FontWeight.BOLD, 48);
+		gc.setFont(theFont);
+		gc.fillText("Hello, World!", 60, 50);
+		gc.strokeText("Hello, World!", 60, 50);
+
+		Image earth = new Image("earth.png");
+		gc.drawImage(earth, 180, 100);
+
+		theStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
