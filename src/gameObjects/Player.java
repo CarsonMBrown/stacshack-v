1b906d35a77e056b150337f1333c099627f9490e
Carson Brown <cmb38@st-andrews.ac.uk>
2019-03-02 16:35:56 +0000

    Max velocity

--- src/gameObjects/Player.java
+++ src/gameObjects/Player.java
@@ -3,6 +3,11 @@ package gameObjects;
 import java.util.ArrayList;
 
 public class Player extends GameObject {
+    private static final int MAX_VELOCITY_X = 10;
+    private static final int MAX_VELOCITY_Y = 10;
+
+    boolean isInAir = false;
+
     public Player() {
         super(100, 100, 50, 50, "assets/player/idle.png");
         // TODO Auto-generated constructor stub
@@ -15,6 +20,10 @@ public class Player extends GameObject {
 
         if (input.contains("RIGHT")) {
             addVelocity(1, 0);
+        }
+
+        if (!isInAir) {
+            setVelocity(Math.min(getVelocityX(), MAX_VELOCITY_X), Math.min(getVelocityY(), MAX_VELOCITY_Y));
         }
     }
 }
