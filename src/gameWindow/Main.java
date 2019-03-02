1a38564e08169516568f9ce1d9b9260444a5923d
Ben Claydon <bc89@st-andrews.ac.uk>
2019-03-02 13:29:16 +0000

    Game Loop Implemented!

--- src/gameWindow/Main.java
+++ src/gameWindow/Main.java
@@ -1,5 +1,6 @@
 package gameWindow;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -7,6 +8,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -21,7 +23,7 @@ public class Main extends Application {
         Scene theScene = new Scene(root);
         theStage.setScene(theScene);
 
-        Canvas canvas = new Canvas(400, 200);
+        Canvas canvas = new Canvas(1000, 1000);
         root.getChildren().add(canvas);
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -34,8 +36,16 @@ public class Main extends Application {
         gc.fillText("Hello, World!", 60, 50);
         gc.strokeText("Hello, World!", 60, 50);
 
-        Image earth = new Image("earth.png");
-        gc.drawImage(earth, 180, 100);
+        Image earth = new Image("doot.jpeg");
+        final long startNanoTime = System.nanoTime();
+        
+        new AnimationTimer() {
+        	public void handle(long currentNanoTime) {
+        		 double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+        		 
+        		 //TODO add game logic here!
+        	}
+        }.start();
 
         theStage.show();
     }
