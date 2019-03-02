1d22a06524561fb330a669d7ab03dc292760c560
Carson Brown <cmb38@st-andrews.ac.uk>
2019-03-02 16:28:05 +0000

    Player movement

--- src/gameWindow/Game.java
+++ src/gameWindow/Game.java
@@ -1,4 +1,5 @@
 package gameWindow;
+
 import java.util.ArrayList;
 
 import gameObjects.GameObject;
@@ -6,49 +7,42 @@ import gameObjects.Player;
 import javafx.scene.canvas.GraphicsContext;
 
 public class Game {
-	
-	private ArrayList<GameObject> objects;
-	private Player player;
-	
-	
-	public Game() {
-		objects = new ArrayList<>();
-		player = new Player();
-	}
-	
-	//Called in updateScreen
-	public void updatePosition(ArrayList<String> input) {
-		if (input.contains("LEFT")) {
-			for (GameObject o : objects) {
-				o.addVelocity(-1, 0);
-			}
-		}
-		
-		if (input.contains("RIGHT")) {
-			for (GameObject o : objects) {
-				o.addVelocity(1, 0);
-			}
-		}
-	}
-	
-	public void updateScreen(double t) {
-		for (GameObject o : objects) {
-			o.update(t);
-			//Once an object touches lava, it gets deleted.
-			//If player deleted, game ends.
-			
-			
-		}
-	}
-	
-	public void addObject(GameObject o) {
-		objects.add(o);
-	}
-	
-	public void renderGame(GraphicsContext gc) {
-		player.render(gc);
-		for (GameObject o : objects) {
-			o.render(gc);
-		}
-	}
+
+    private ArrayList<GameObject> objects;
+    private Player player;
+
+    public Game() {
+        objects = new ArrayList<>();
+        player = new Player();
+    }
+
+    // Called in updateScreen
+    public void updatePosition(ArrayList<String> input) {
+        player.handleInput(input);
+        
+    }
+
+    public void updateScreen(double t) {
+        for (GameObject o : objects) {
+            o.update(t);
+            // Once an object touches lava, it gets deleted.
+            // If player deleted, game ends.
+
+        }
+    }
+
+    public void addObject(GameObject o) {
+        objects.add(o);
+    }
+
+    public void renderGame(GraphicsContext gc) {
+        player.render(gc);
+        for (GameObject o : objects) {
+            o.render(gc);
+        }
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
 }
