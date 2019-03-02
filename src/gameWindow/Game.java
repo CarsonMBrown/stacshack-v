893bd2c3573d9206faa1f3a14c828e9a75120f9d
Ben Claydon <bc89@st-andrews.ac.uk>
2019-03-02 22:45:36 +0000

    Waveform?!

--- src/gameWindow/Game.java
+++ src/gameWindow/Game.java
@@ -13,7 +13,7 @@ public class Game {
     private ArrayList<GameObject> objects;
     private Player player;
     private ArrayList<Block> blocks;
-    private static int displacement;
+    private static int updateCycle;
     private int score = 0;
     private static final double SCORE_MULTIPLIER = 1.0 / 2000;
 
@@ -25,6 +25,10 @@ public class Game {
         blocks.add(new Block(100, 300));
     }
 
+    public ArrayList<Block> getBlocks() {
+    	return blocks;
+    }
+    
     // Called in updateScreen
     public void updatePosition(ArrayList<String> input) {
         player.handleInput(input);
@@ -32,12 +36,11 @@ public class Game {
     }
 
     public boolean countBlocks(double t) {
-        displacement += player.getVelocityX() * t;
-        if (displacement >= Generator.BLOCK_SIZE) {
-            displacement = 0;
-            return true;
-        }
-        return false;
+    	updateCycle++;
+    	if (updateCycle % 7 == 0) {
+    		return true;
+    	}
+    	return false;
     }
 
     public void updateScreen(double t) {
--- src/terrain/Generator.java
+++ src/terrain/Generator.java
@@ -2,22 +2,35 @@ package terrain;
 
 import gameObjects.Block;
 import gameWindow.Game;
+import gameWindow.Main;
 
 public class Generator {
-	public static final int BLOCK_SIZE = 50;
+	public static final int BLOCK_SIZE = 15;
 	private static int colNumber = 0;
-	
-	
+
 	public static void addCol(Game g, int height, int width) {
-		int blockHeight = (int) Math.ceil(height/BLOCK_SIZE);
-		int blockGap = 6;
-		int mp = (int)(blockHeight * Math.sin(colNumber * Math.PI / 10));
-		//The ceiling height in blocks
-		for (int i = 0; i < blockHeight; i++) {
-			if (i != Math.abs(mp - blockGap)) {
-				g.addBlock(new Block(width + 600, i * BLOCK_SIZE));
+		/*
+		 * double lastBlockX = g.getBlocks().get(g.getBlocks().size() - 1).getX(); int
+		 * blockHeight = (int) Math.ceil(height/BLOCK_SIZE ); int blockGap = 0; int mp =
+		 * (int)((blockHeight / 4) * Math.sin(colNumber * Math.PI/100)); //The ceiling
+		 * height in blocks
+		 */
+
+		double lastBlockX = g.getBlocks().get(g.getBlocks().size() - 1).getX();
+		int blocksInCol = (int) Math.ceil(height / BLOCK_SIZE);
+		int midBlock = (int) ((blocksInCol / 16) * Math.sin(colNumber * Math.PI / 50) + (blocksInCol / 4));
+		int gap = 10;
+		int randomness = 2;
+
+		for (int i = 0; i < blocksInCol; i++) {
+			int lb = (int) (midBlock - gap - Math.random() * randomness);
+			int ub = (int) (midBlock + gap + Math.random() * randomness);
+			if (i < lb || i > ub) {
+				g.addBlock(new Block((int) lastBlockX + BLOCK_SIZE, i * BLOCK_SIZE));
 			}
 		}
+		g.addBlock(new Block((int) lastBlockX + BLOCK_SIZE, 0));
+		g.addBlock(new Block((int) lastBlockX + BLOCK_SIZE, Main.SCREEN_HEIGHT - BLOCK_SIZE));
 		colNumber++;
 	}
 }
