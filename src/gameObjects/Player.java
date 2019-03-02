0c1bf23ad35ae90e0f3995d3ce1b92f4883e4326
Carson Brown <cmb38@st-andrews.ac.uk>
2019-03-02 15:28:08 +0000

    Git weirdness

--- src/gameObjects/Player.java
+++ src/gameObjects/Player.java
@@ -6,14 +6,17 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 
-public class Player {
-	private int x, y, vX, vY;
-	
+public class Player extends GameObject {
+	public Player(int x, int y, int width, int height, String imgPath) {
+		super(x, y, width, height, imgPath);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void handleInput(String keyCode) {
 		
 	}
 	
-	private void updatePosition(ArrayList<String> input) {
+	public void updatePosition(ArrayList<String> input) {
 		if (input.contains("LEFT")) {
 			//Increase left velocity vector
 		}
@@ -21,48 +24,5 @@ public class Player {
 		if (input.contains("RIGHT")) {
 			//Increase right velocity vector
 		}
-	}
-	private int x;
-	private int y;
-	
-	
-	
-	public int getX() {
-		return x;
-	}
-	private int x;
-	private int y;
-	
-	
-	
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public int getvX() {
-		return vX;
-	}
-
-	public void setvX(int vX) {
-		this.vX = vX;
-	}
-
-	public int getvY() {
-		return vY;
-	}
-
-	public void setvY(int vY) {
-		this.vY = vY;
-	}
-	
-	
-	
+	}	
 }
