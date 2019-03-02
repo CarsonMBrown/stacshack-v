ddac8ab746dd447f35731eacfa83902709a2fa6a
Carson Brown <cmb38@st-andrews.ac.uk>
2019-03-02 14:02:35 +0000

    Push

--- src/gameWindow/Main.java
+++ src/gameWindow/Main.java
@@ -1,5 +1,6 @@
 package gameWindow;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -7,6 +8,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -15,13 +17,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage theStage) throws Exception {
-        theStage.setTitle("Canvas Example");
+        theStage.setTitle("Cave Adventure");
 
         Group root = new Group();
-        Scene theScene = new Scene(root);
-        theStage.setScene(theScene);
+        Scene scene = new Scene(root);
+        theStage.setScene(scene);
 
-        Canvas canvas = new Canvas(400, 200);
+        Canvas canvas = new Canvas(1000, 1000);
         root.getChildren().add(canvas);
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -29,14 +31,18 @@ public class Main extends Application {
         gc.setFill(Color.RED);
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
-        Font theFont = Font.font("Times New Roman", FontWeight.BOLD, 48);
-        gc.setFont(theFont);
-        gc.fillText("Hello, World!", 60, 50);
-        gc.strokeText("Hello, World!", 60, 50);
+        Font timesNewRoman = Font.font("Times New Roman", FontWeight.BOLD, 48);
 
-        Image earth = new Image("earth.png");
-        gc.drawImage(earth, 180, 100);
-
+        final long startNanoTime = System.nanoTime();
+        
+        new AnimationTimer() {
+        	public void handle(long currentNanoTime) {
+        		 double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+        		 
+        		 //TODO add game logic here!
+        	}
+        }.start();
+        
         theStage.show();
     }
 
